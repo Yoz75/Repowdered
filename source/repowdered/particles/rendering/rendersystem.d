946dce97a2ea9c94b603651edc4ce68d9cca7f4e
yoz75 <levtannn@gmail.com>
2026-06-23 16:37:52 +0300

    uninited sprite is now like a chess board

--- source/repowdered/particles/rendering/rendersystem.d
+++ source/repowdered/particles/rendering/rendersystem.d
@@ -105,7 +105,7 @@ public final class MapRenderSystem
     {
         foreach(i, ref pixel; spriteBuffer)
         {
-            if(i & 1)
+            if(i % 3 == 0)
             {
                 pixel = Color32(0);
             }
