e4d0f23cc3039240acea6550e17dd08c9389c97e
yoz75 <levtannn@gmail.com>
2026-06-25 19:41:40 +0300

    Update meta.d

--- source/repowdered/particles/meta.d
+++ source/repowdered/particles/meta.d
@@ -5,12 +5,22 @@ import cereslib.jsonutils;
 
 
 /// The name of particle type
-public struct TypeName // Remember: components without @Component attribute don't delete when particle is remoevd, we add the once!
+public struct TypeName // Remember: components without @Component attribute don't delete when particle is removed, we add the once!
 {
     mixin MakeJsonizable;
 public:
     /// The name of the particle type. For example, Repowdered.FLuids.Water
     string name;
+
+    bool opEquals(const TypeName other) const
+    {
+        return name == other.name;
+    }
+
+    size_t toHash() const @nogc @safe pure nothrow
+    {
+        return name.hashOf;
+    }
 }
 
 /// Marks that current entity is a hollow particle. Add this component to make it act as air
